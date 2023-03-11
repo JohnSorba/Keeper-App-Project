d0aa92b7d9bd3fa92ae55077bf66e98d4636f5a0
Daniel Ndanema <dannytech001@gmail.com>
2023-03-11 23:10:20 +0800

    reinstalled all npm and react packages/dependencies

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,5 +3,5 @@ import React from "react";
 export default function Footer() {
   const year = new Date().getFullYear;
 
-  return <footer>Copyright &ncopy; {year};</footer>;
+  return <footer>Copyright &ncopy; {year}</footer>;
 }
